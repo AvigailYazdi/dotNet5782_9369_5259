50f26d0cf25b99a46e826c76e2e11a02000cf1dc
Avigail Yazdi <ayazdi@g.jct.ac.il>
2021-10-10 10:56:48 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program9369.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9369();
+            Welcome5259();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5259();
+
+        private static void Welcome9369()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
