704e15ea5349892129f2f7f3c32825ce036221f6
Shilat Shimon <sshimon@g.jct.ac.il>
2021-10-20 10:39:22 +0300

    struct_2

---
+++ DAL/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight { get; set; }
+            public DroneStatuses Status { get; set; }
+            public double Battery { get; set; }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+        }
+    }
+ }
