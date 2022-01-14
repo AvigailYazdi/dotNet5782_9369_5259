f39ed6e4e20905ee8d963774e232ff7ee54ca73e
Shilat Shimon <sshimon@g.jct.ac.il>
2022-01-14 13:25:18 +0200

    step3

--- BL1/BL/BL.cs
+++ BL1/BL/BL.cs
@@ -29,9 +29,9 @@ namespace BL
             dList = new List<BO.DroneToL>();
 
             BO.DroneToL d;
-            foreach (var item in dl.ListDrone())
+            try
             {
-                try
+                foreach (var item in dl.ListDrone())
                 {
                     d = new BO.DroneToL()
                     {
@@ -43,6 +43,9 @@ namespace BL
                         CurrentPlace = new BO.Location(),
                         ParcelId = dl.GetConnectParcel(item.Id)
                     };
+                    IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
+                    DO.BaseStation stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
+                    d.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
                     if (dl.checkDc(item.Id))
                     {
                         d.Status = BO.DroneStatus.Maintenance;
@@ -55,15 +58,15 @@ namespace BL
                         switch (GetParcelStatus(d.ParcelId))
                         {
                             case BO.ParcelStatus.Connected:
-                                BO.Location c= GetCustomer(GetParcel(d.ParcelId).Sender.Id).Place;
+                                DO.Customer c = dl.GetCustomer(dl.GetParcel(d.ParcelId).SenderId);
                                 d.CurrentPlace = new BO.Location { Longitude = c.Longitude, Latitude = c.Latitude };
                                 break;
                             case BO.ParcelStatus.PickedUp:
-                                BO.Location c1 = GetCustomer(GetParcel(d.ParcelId).Sender.Id).Place;
+                                DO.Customer c1 = dl.GetCustomer(dl.GetParcel(d.ParcelId).SenderId);
                                 d.CurrentPlace = new BO.Location { Longitude = c1.Longitude, Latitude = c1.Latitude };
                                 break;
                             case BO.ParcelStatus.Provided:
-                                DO.Customer targetDo = dl.GetCustomer(GetParcel(d.ParcelId).Receiver.Id);
+                                DO.Customer targetDo = dl.GetCustomer(dl.GetParcel(d.ParcelId).TargetId);
                                 DO.BaseStation b = closeStation(targetDo.Longitude, targetDo.Latitude);
                                 d.CurrentPlace = new BO.Location { Longitude = b.Longitude, Latitude = b.Latitude };
                                 break;
@@ -71,95 +74,95 @@ namespace BL
                                 break;
                         }
                     }
-                    else
-                    {
-                        IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
-                        DO.BaseStation stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
-                        d.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
-                    }
+                    //else
+                    //{
+                    //    IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
+                    //    DO.BaseStation stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
+                    //    d.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
+                    //}
                     dList.Add(d);
                 }
-                catch (DO.MissingIdException ex)
-                {
-                    throw new BO.MissingIdException(ex.Id, ex.EntityName);
-                }
+            }
+            catch (DO.MissingIdException ex)
+            {
+                throw new BO.MissingIdException(ex.Id, ex.EntityName);
             }
 
 
-                //dList = (from item in dl.ListDrone()//dList
-                //         select new BO.DroneToL()
-                //         {
-                //             Id = item.Id,
-                //             Model = item.Model,
-                //             Weight = (BO.WeightCategories)item.MaxWeight,
-                //             Battery = rand.Next(20 * 100, 40 * 100) / 100.0,
-                //             Status = BO.DroneStatus.Avaliable,
-                //             CurrentPlace = new BO.Location(),
-                //             ParcelId = dl.GetConnectParcel(item.Id)
-                //         }).ToList();
-                //DO.BaseStation stationDo;
-                //double distance1, distance2;
-                //try
-                //{
-                //    foreach (var item in dList.ToList())
-                //    {
-                //        BO.DroneToL temp = GetDroneToL(item.Id);
-                //        if (temp.ParcelId > 0 && dl.GetParcel(temp.ParcelId).Delivered == null)
-                //        {
-                //            DO.Parcel p = dl.GetParcel(temp.ParcelId);
-                //            temp.Status = BO.DroneStatus.Delivery;
-                //            DO.Customer senderDo = dl.GetCustomer(p.SenderId);
-                //            DO.Customer targetDo = dl.GetCustomer(p.TargetId);
-                //            if (p.PickedUp == null)
-                //            {
-                //                stationDo = closeStation(senderDo.Longitude, senderDo.Latitude);
-                //                temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
-                //            }
-                //            else
-                //                temp.CurrentPlace = new BO.Location() { Longitude = senderDo.Longitude, Latitude = senderDo.Latitude };
-                //            distance1 = dl.DistanceInKm(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude, targetDo.Longitude, targetDo.Latitude);
-                //            stationDo = closeStation(targetDo.Longitude, targetDo.Latitude);
-                //            distance2 = dl.DistanceInKm(targetDo.Longitude, targetDo.Latitude, stationDo.Longitude, stationDo.Latitude);
-                //            temp.Battery = rand.Next((int)(getBattery(distance1 + distance2, temp.Id) * 100), 100 * 100) / 100.0;
-                //        }
-                //        else if (temp.ParcelId <= 0)
-                //        {
-                //            temp.Status = (BO.DroneStatus)rand.Next(0, 2);
-                //        }
-                //        if (temp.Status == BO.DroneStatus.Maintenance)
-                //        {
-                //            IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
-                //            stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
-                //            temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
-                //            temp.Battery = rand.Next(0, 20 * 100) / 100.0;
-                //            dl.UpdateChargeDrone(temp.Id, stationDo.Id);
-                //        }
-                //        else if (temp.Status == BO.DroneStatus.Avaliable)
-                //        {
-                //            IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
-                //            stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
-                //            temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
-                //            IEnumerable<DO.Parcel> p = dl.GetParcelsByPerdicate(it => it.Delivered != null);
-                //            if (p.Count() != 0)
-                //            {
-                //                DO.Parcel pc = p.ElementAtOrDefault(rand.Next(0, p.Count()));
-                //                temp.CurrentPlace = new BO.Location() { Longitude = dl.GetCustomer(pc.TargetId).Longitude, Latitude = dl.GetCustomer(pc.TargetId).Latitude };
-                //                stationDo = closeStation(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude);
-                //                distance1 = dl.DistanceInKm(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude, stationDo.Longitude, stationDo.Latitude);
-                //                temp.Battery = rand.Next((int)(getBattery(distance1, stationDo.Id) * 100), 100 * 100) / 100.0;
-                //                // dl.UpdateChargeDrone(temp.Id, stationDo.Id);
-                //            }
-                //        }
-                //        UpdateDroneToL(temp);
-                //    }
-                //}
-                //catch (DO.MissingIdException ex)
+            //dList = (from item in dl.ListDrone()//dList
+            //         select new BO.DroneToL()
+            //         {
+            //             Id = item.Id,
+            //             Model = item.Model,
+            //             Weight = (BO.WeightCategories)item.MaxWeight,
+            //             Battery = rand.Next(20 * 100, 40 * 100) / 100.0,
+            //             Status = BO.DroneStatus.Avaliable,
+            //             CurrentPlace = new BO.Location(),
+            //             ParcelId = dl.GetConnectParcel(item.Id)
+            //         }).ToList();
+            //DO.BaseStation stationDo;
+            //double distance1, distance2;
+            //try
+            //{
+            //    foreach (var item in dList.ToList())
+            //    {
+            //        BO.DroneToL temp = GetDroneToL(item.Id);
+            //        if (temp.ParcelId > 0 && dl.GetParcel(temp.ParcelId).Delivered == null)
+            //        {
+            //            DO.Parcel p = dl.GetParcel(temp.ParcelId);
+            //            temp.Status = BO.DroneStatus.Delivery;
+            //            DO.Customer senderDo = dl.GetCustomer(p.SenderId);
+            //            DO.Customer targetDo = dl.GetCustomer(p.TargetId);
+            //            if (p.PickedUp == null)
+            //            {
+            //                stationDo = closeStation(senderDo.Longitude, senderDo.Latitude);
+            //                temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
+            //            }
+            //            else
+            //                temp.CurrentPlace = new BO.Location() { Longitude = senderDo.Longitude, Latitude = senderDo.Latitude };
+            //            distance1 = dl.DistanceInKm(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude, targetDo.Longitude, targetDo.Latitude);
+            //            stationDo = closeStation(targetDo.Longitude, targetDo.Latitude);
+            //            distance2 = dl.DistanceInKm(targetDo.Longitude, targetDo.Latitude, stationDo.Longitude, stationDo.Latitude);
+            //            temp.Battery = rand.Next((int)(getBattery(distance1 + distance2, temp.Id) * 100), 100 * 100) / 100.0;
+            //        }
+            //        else if (temp.ParcelId <= 0)
+            //        {
+            //            temp.Status = (BO.DroneStatus)rand.Next(0, 2);
+            //        }
+            //        if (temp.Status == BO.DroneStatus.Maintenance)
+            //        {
+            //            IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
+            //            stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
+            //            temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
+            //            temp.Battery = rand.Next(0, 20 * 100) / 100.0;
+            //            dl.UpdateChargeDrone(temp.Id, stationDo.Id);
+            //        }
+            //        else if (temp.Status == BO.DroneStatus.Avaliable)
+            //        {
+            //            IEnumerable<DO.BaseStation> bs = dl.GetStationsByPerdicate(s => s.ChargeSlots != 0);
+            //            stationDo = bs.ElementAtOrDefault(rand.Next(0, bs.Count()));
+            //            temp.CurrentPlace = new BO.Location() { Longitude = stationDo.Longitude, Latitude = stationDo.Latitude };
+            //            IEnumerable<DO.Parcel> p = dl.GetParcelsByPerdicate(it => it.Delivered != null);
+            //            if (p.Count() != 0)
+            //            {
+            //                DO.Parcel pc = p.ElementAtOrDefault(rand.Next(0, p.Count()));
+            //                temp.CurrentPlace = new BO.Location() { Longitude = dl.GetCustomer(pc.TargetId).Longitude, Latitude = dl.GetCustomer(pc.TargetId).Latitude };
+            //                stationDo = closeStation(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude);
+            //                distance1 = dl.DistanceInKm(temp.CurrentPlace.Longitude, temp.CurrentPlace.Latitude, stationDo.Longitude, stationDo.Latitude);
+            //                temp.Battery = rand.Next((int)(getBattery(distance1, stationDo.Id) * 100), 100 * 100) / 100.0;
+            //                // dl.UpdateChargeDrone(temp.Id, stationDo.Id);
+            //            }
+            //        }
+            //        UpdateDroneToL(temp);
+            //    }
+            //}
+            //catch (DO.MissingIdException ex)
 
-                //{
-                //    throw new BO.MissingIdException(ex.Id, ex.EntityName);
-                //}
+            //{
+            //    throw new BO.MissingIdException(ex.Id, ex.EntityName);
+            //}
 
-            }
+        }
         /// <summary>
         /// A function that returns the station is the closet to the location
         /// </summary>
